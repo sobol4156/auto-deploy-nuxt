12f672ddf15c9e1be73cc00aad227267138e3d54
DESKTOP-6CDM0P7\sobol <sobol4156@gmail.com>
2024-11-30 15:25:59 +0300

    feat: added example routing

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="layout">
+    <header>
+      <NuxtLink to="/">home</NuxtLink>
+      <NuxtLink to="/second">second</NuxtLink>
+    </header>
+    <div>
+      <NuxtPage />
+    </div>
+  </div>
+</template>
---
+++ pages/second.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    роут second
+  </div>
+</template>
