7a09ee05953cd590f130487c34b37dd91d6f4724
DESKTOP-6CDM0P7\sobol <sobol4156@gmail.com>
2024-12-01 01:09:49 +0300

    fix: yml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  devServer: {
+    port: 3001, 
+    host: '0.0.0.0'
+  }
 })
