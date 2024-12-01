f145a6510993ac4084a5341c2832056294876e03
DESKTOP-6CDM0P7\sobol <sobol4156@gmail.com>
2024-12-01 19:54:14 +0300

    fix: yml

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="layout">
     <header>
-      <NuxtLink to="/">home 1</NuxtLink>
-      <NuxtLink to="/second">second</NuxtLink>
+      <NuxtLink to="/">home 1 </NuxtLink>
+      <NuxtLink to="/second"> second 2</NuxtLink>
     </header>
     <main>
       <NuxtPage />
